db672d182909a0f7684a781a453242913c439a35
Amalie Smidth <amaliesmidth@gmail.com>
2018-10-17 10:05:04 +0100

    added controller methods

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,12 @@
 class AuthorsController < ApplicationController
+
+  before_action :set_author, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @authors = Author.all
+  end
+
   def show
-    @author = Author.find(params[:id])
   end
 
   def new
@@ -8,12 +14,29 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
+    if @author = Author.create(author_params)
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+  end
 
-    redirect_to author_path(@author)
+  def edit
+  end
+
+  def update
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
   end
 
   private
+
+  def set_author
+    @author = Author.find(params[:id])
+  end
 
   def author_params
     params.permit(:name, :email, :phone_number)
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,42 @@
 class PostsController < ApplicationController
+
+  before_action :set_posts, only: [:show, :edit, :update]
+
+  def index
+    @posts = Post.all
+  end
+
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    if @post.create(post_params)
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end 
   end
 
   private
+
+  def set_posts
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.permit(:title, :category, :content)
